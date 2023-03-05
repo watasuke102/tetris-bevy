1e3e6ae87dc65a0360cc986d2c673d73d63874a8
watasuke102 <watasuke102@gmail.com>
2023-03-06 07:57:17 +0800

    update: remove timer clock function
    call` timer::tick` in `move_block()` instead

--- src/block.rs
+++ src/block.rs
@@ -14,22 +14,19 @@ pub fn init(app: &mut App) {
   app.add_startup_system(startup);
   app.add_system(move_block);
 
-  app.add_system(block_drop_timer);
   app.insert_resource(BlockDropTimer(Timer::new(
     std::time::Duration::from_millis(1000),
     TimerMode::Repeating,
   )));
 }
 
-fn block_drop_timer(mut timer: ResMut<BlockDropTimer>, time: Res<Time>) {
-  timer.0.tick(time.delta());
-}
-
 fn move_block(
   mut query: Query<&mut Block>,
   mut field_query: Query<(&mut Sprite, &mut field::Field)>,
-  timer: Res<BlockDropTimer>,
+  mut timer: ResMut<BlockDropTimer>,
+  time: Res<Time>,
 ) {
+  timer.0.tick(time.delta());
   if !timer.0.finished() {
     return;
   }
