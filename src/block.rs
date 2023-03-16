5bfaa535b88a5c71c8d58ebb1db7cabcd0689dca
watasuke102 <watasuke102@gmail.com>
2023-03-16 14:39:04 +0800

    update: add `is_movable_pos()` to Field

--- src/block.rs
+++ src/block.rs
@@ -16,7 +16,7 @@ const MINO_TYPES: [[IVec2; 4]; 7] = [
 ];
 
 #[derive(Component, Default)]
-struct Mino {
+pub struct Mino {
   pos:       IVec2,
   mino_type: usize,
 }
@@ -25,13 +25,14 @@ impl Mino {
   fn set_type(
     &mut self,
     mino_type: usize,
-    mut field_query: &mut Query<(&mut Sprite, &mut field::Field)>,
+    mut field: ResMut<field::Field>,
+    mut field_block_query: &mut Query<(&mut Sprite, &mut field::FieldBlock)>,
   ) {
     self.pos = IVec2::new(5, 5);
     self.mino_type = mino_type;
     for e in MINO_TYPES[self.mino_type] {
-      field::set_block(
-        &mut field_query,
+      field.set_block(
+        &mut field_block_query,
         self.pos + e,
         Color::hex("98c379").unwrap(),
       );
@@ -43,7 +44,6 @@ impl Mino {
 struct MinoDropTimer(Timer);
 
 pub fn init(app: &mut App) {
-  app.add_startup_system(startup);
   app.add_system(move_mino);
 
   app.insert_resource(MinoDropTimer(Timer::new(
@@ -54,7 +54,8 @@ pub fn init(app: &mut App) {
 
 fn move_mino(
   mut query: Query<&mut Mino>,
-  mut field_query: Query<(&mut Sprite, &mut field::Field)>,
+  mut field: ResMut<field::Field>,
+  mut field_block_query: Query<(&mut Sprite, &mut field::FieldBlock)>,
   mut timer: ResMut<MinoDropTimer>,
   time: Res<Time>,
 ) {
@@ -64,26 +65,34 @@ fn move_mino(
   }
 
   let mut mino = query.single_mut();
-  if mino.pos.y == field::FIELD_ROW - 1 {
+  if !field.is_movable_pos(&mino.pos) {
     let mino_type = mino.mino_type;
-    mino.set_type((mino_type + 1) % MINO_TYPES.len(), &mut field_query);
+    mino.set_type(
+      (mino_type + 1) % MINO_TYPES.len(),
+      field,
+      &mut field_block_query,
+    );
     return;
   }
   for e in MINO_TYPES[mino.mino_type] {
-    field::unset_block(&mut field_query, mino.pos + e);
+    field.unset_block(&mut field_block_query, mino.pos + e);
   }
   mino.pos.y += 1;
   for e in MINO_TYPES[mino.mino_type] {
-    field::set_block(
-      &mut field_query,
+    field.set_block(
+      &mut field_block_query,
       mino.pos + e,
       Color::hex("98c379").unwrap(),
     );
   }
 }
 
-fn startup(mut commands: Commands, mut field_query: Query<(&mut Sprite, &mut field::Field)>) {
+pub fn startup_mino(
+  mut commands: Commands,
+  field: ResMut<field::Field>,
+  mut field_block_query: Query<(&mut Sprite, &mut field::FieldBlock)>,
+) {
   let mut mino = Mino::default();
-  mino.set_type(0, &mut field_query);
+  mino.set_type(0, field, &mut field_block_query);
   commands.spawn(mino);
 }
--- src/field.rs
+++ src/field.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 
+use crate::block;
+
 const BLOCK_SIZE: f32 = 24.0;
 const FIELD_X: f32 = -200.0;
 const FIELD_Y: f32 = 400.0;
@@ -7,17 +9,77 @@ pub const FIELD_ROW: i32 = 20 + 5;
 const FIELD_COLUMN: i32 = 10;
 
 #[derive(Component, Default)]
-pub struct Field {
+pub struct FieldBlock {
   pos:       IVec2,
   has_block: bool,
   visible:   bool,
 }
 
+#[derive(Resource, Default, Clone, Copy)]
+pub struct Field {
+  blocks: [[bool; FIELD_ROW as usize]; FIELD_COLUMN as usize],
+}
+
+impl Field {
+  pub fn is_movable_pos(&self, pos: &IVec2) -> bool {
+    if pos.y == FIELD_ROW - 1 {
+      return false;
+    }
+    if self.blocks[pos.x as usize][pos.y as usize + 1] {
+      return false;
+    }
+    true
+  }
+
+  pub fn set_block(
+    &mut self,
+    query: &mut Query<(&mut Sprite, &mut FieldBlock)>,
+    pos: IVec2,
+    color: Color,
+  ) -> bool {
+    self.blocks[pos.x as usize][pos.y as usize] = true;
+    for (mut sprite, mut field) in query {
+      if field.pos != pos {
+        continue;
+      }
+      if !field.visible {
+        break;
+      }
+      field.has_block = true;
+      sprite.color = color;
+      break;
+    }
+
+    if pos.y == FIELD_ROW - 1 {
+      return true;
+    }
+    return false;
+  }
+
+  pub fn unset_block(&mut self, query: &mut Query<(&mut Sprite, &mut FieldBlock)>, pos: IVec2) {
+    self.blocks[pos.x as usize][pos.y as usize] = false;
+    for (mut sprite, mut field) in query {
+      if field.pos != pos {
+        continue;
+      }
+      if !field.visible {
+        break;
+      }
+      field.has_block = false;
+      sprite.color = Color::hex("282c34").unwrap();
+      break;
+    }
+  }
+}
+
 pub fn init(app: &mut App) {
   app.add_startup_system(startup);
+  app.add_system(startup_mino_after_field_init);
 }
 
 fn startup(mut commands: Commands) {
+  commands.init_resource::<Field>();
+
   for i in 0..FIELD_COLUMN {
     for j in 0..FIELD_ROW {
       let visible = j >= 5;
@@ -35,7 +97,7 @@ fn startup(mut commands: Commands) {
           ),
           ..default()
         },
-        Field {
+        FieldBlock {
           pos:       IVec2 { x: i, y: j },
           has_block: false,
           visible:   visible,
@@ -45,35 +107,13 @@ fn startup(mut commands: Commands) {
   }
 }
 
-pub fn set_block(query: &mut Query<(&mut Sprite, &mut Field)>, pos: IVec2, color: Color) -> bool {
-  for (mut sprite, mut field) in query {
-    if field.pos != pos {
-      continue;
-    }
-    if !field.visible {
-      break;
-    }
-    field.has_block = true;
-    sprite.color = color;
-    break;
-  }
-
-  if pos.y == FIELD_ROW - 1 {
-    return true;
-  }
-  return false;
-}
-
-pub fn unset_block(query: &mut Query<(&mut Sprite, &mut Field)>, pos: IVec2) {
-  for (mut sprite, mut field) in query {
-    if field.pos != pos {
-      continue;
-    }
-    if !field.visible {
-      break;
-    }
-    field.has_block = false;
-    sprite.color = Color::hex("282c34").unwrap();
-    break;
+fn startup_mino_after_field_init(
+  commands: Commands,
+  query: Query<&block::Mino>,
+  field: ResMut<Field>,
+  field_block_query: Query<(&mut Sprite, &mut FieldBlock)>,
+) {
+  if let Err(_) = query.get_single() {
+    block::startup_mino(commands, field, field_block_query);
   }
 }
